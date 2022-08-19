83ccd0b221eb0d171192a00f2967a15d72599159
Serge Labada <sergelabada@gmail.com>
2022-08-19 16:56:07 +0300

    Studying exercices

---
+++ app/src/main/java/com/example/lesson06networkoperationsexercices/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.lesson06networkoperationsexercices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lesson06networkoperationsexercices.databinding.ActivityMainBinding
+import com.example.lesson06networkoperationsexercices.databinding.ItemBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        setContentView(binding.root)
+
+        val items = List(20) {
+            "item $it"
+        }
+
+        with(binding) {
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            recyclerView.adapter = ItemAdapter(items)
+        }
+    }
+}
+
+class ItemViewHolder(private val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: String) {
+        binding.textView.text = item
+    }
+}
+
+class ItemAdapter(private val list: List<String>) : RecyclerView.Adapter<ItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val context = parent.context
+        val layoutInflater = LayoutInflater.from(context)
+        val binding = ItemBinding.inflate(layoutInflater, parent, false)
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
